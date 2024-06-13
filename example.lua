7730296c714320cb24ccc6c56820cd1bc679dfec
william-riebel <william.riebel@protonmail.com>
2024-06-13 22:59:48 +0200

    First version of the code
    Library to style output to the terminal.

---
+++ example.lua
@@ -0,0 +1,101 @@
+-- An example script to show off the functions of this module.
+-- To use this module, copy the required files to your project folder and include them in your code with "require".
+-- Like this:
+require("style")
+
+-- Now you are ready to use the functions that this module provides.
+
+
+-- When styling Text using this module you will need a "style table".
+-- The style table contains all the necessary information to display your text.
+-- It contains the text color, the background color and if the text is bold, italic, underlined, double underlined and inversed.
+-- The name of the style table does not matter but the names of the variables it contains does.
+-- A style table looks like this:
+
+example_style = {
+    fg = {255, 255, 0},        -- Foreground or text color (string or a table containing three nubers 0-255)
+    bg = "cyan",               -- Background color         (string or a table containing three nubers 0-255)
+    bold = true,               -- Bold or not              (boolian)
+    italic = false,            -- Italic or not            (boolian)
+    underline = false,         -- Underlined or not        (boolian)
+    double_underline = false,  -- Double underlined or not (boolian)
+    struck = false,
+    inverse = false
+    }
+
+-- Notice that there are two ways colors are represented here.
+-- Firstly, there is the color beeing represented by three numbers from 0 to 255.
+-- This works intuitively, just like you would expect.
+-- The three numbers represent the red, green and blue color channels.
+--
+-- Secondly, there is a string containing the name of a color.
+-- There are eight predetermined colors and their bright variants.
+--     "black"
+--     "red"
+--     "green"
+--     "yellow"
+--     "blue"
+--     "magenta"
+--     "cyan"
+--     "white"
+--     "bright_black" ((dark grey), just named "bright_black" for consitency)
+--     "bright_red"
+--     "bright_green"
+--     "bright_yellow"
+--     "bright_blue"
+--     "bright_magenta"
+--     "bright_cyan"
+--     "bright_white"
+--
+--     "default" (this is just the terminals default color scheme but is treated just like a color)
+--
+-- Notice that the way in which these colors are displayed is determined by the terminal that is displaying them.
+-- If, for some reason, someones terminal displays "green" with a color that isnt green this program will also use this color instead of green.
+-- This usually does not matter but if you need a color to be represented accurately you can just use the other method of giving the exact color.
+--
+--
+-- For greater convenience when making style tables you can use the "create_style" function.
+-- It is not necessary to use this function when creating a style table, it is just convenient since it names all the variables contained in the style table correctly.
+
+second_example_style = create_style("red", "default", false, false, true, false, true, false)
+--                                    |        |        |      |      |     |      |     |
+--                                    |        |        |      |      |     |      |     |
+--                    [foreground color]       |        |      |      |     |      |     |
+--                                             |        |      |      |     |      |     |
+--                             [background color]       |      |      |     |      |     |
+--                                                      |      |      |     |      |     |
+--                                                  [bold]     |      |     |      |     |
+--                                                             |      |     |      |     |
+--                                                       [italic]     |     |      |     |
+--                                                                    |     |      |     |
+--                                                           [underline]    |      |     |
+--                                                                          |      |     |
+--                                                          [double underline]     |     |
+--                                                                                 |     |
+--                                                                          [inverse]    |
+--                                                                                       |
+--                                                                         [strike through]
+
+
+-- Now that you know how to make correct style tables, let us use those tables to make some colored text.
+-- Since this module works by just adding some special characters to a string, you can convert any string into a string with styling by using the "convert_style" function.
+-- Here is some example code, I recommend executing it in your terminal to see what it does.
+
+example_string = "Just some text that is about to be styled."
+
+print(example_string) -- before using the convert_style function
+
+example_string = convert_style(example_string, example_style) -- using the convert_style function on example_string with example_style
+
+print(example_string) -- after using the convert_style function
+
+-- For more convenience you can also use the "print_style" function if you just want to print a single string in a single style.
+-- This does not allow for as much freedom as the method mentioned above but it is convenient and works just as well.
+
+print_style("I am lazy", example_style)
+
+-- That is about it for the explanation of the current set of functions that this module provides. Now I would like to show of some funky text to get you inspired.
+-- For example, you can mix different styles in a single string since the convert style function is implemented in a way, where it just adds some special characters at the beginning and end of a string.
+-- Just look at the output of this:
+
+print("Just some regular text with an " .. convert_style("important", second_example_style) .. " word.\27[")
---
+++ style.lua
@@ -0,0 +1,125 @@
+-- Customize output to the terminal using ANSI escape sequences
+-- Written by William-Riebel 13.6.2024
+function create_style(foreground_color, background_color, is_bold, is_italic, is_underlined, is_double_underlined, is_inverse, is_struck) -- A function for convenience when creating style tables (names everything correctly).
+    return {fg = foreground_color, bg = background_color, bold = is_bold, italic = is_italic, underline = is_underlined, double_underline = is_double_underlined, inverse = is_inverse, struck = is_struck}
+end
+
+
+function convert_style(string, style) -- Adds ANSI control codes to a string to make it appear like defined in the style table
+    local fgstring, bgstring = "", ""
+    local formatstring = ""
+
+
+    -- ANSI control codes to customize the text color
+        -- color codes preconfigured in the terminal
+    if (style["fg"] == "default") then
+        fgstring = "\27[39m"
+    elseif (style["fg"] == "black") then
+        fgstring = "\27[30m"
+    elseif (style["fg"] == "red") then
+        fgstring = "\27[31m"
+    elseif (style["fg"] == "green") then
+        fgstring = "\27[32m"
+    elseif (style["fg"] == "yellow") then
+        fgstring = "\27[33m"
+    elseif (style["fg"] == "blue") then
+        fgstring = "\27[34m"
+    elseif (style["fg"] == "magenta") then
+        fgstring = "\27[35m"
+    elseif (style["fg"] == "cyan") then
+        fgstring = "\27[36m"
+    elseif (style["fg"] == "white") then
+        fgstring = "\27[37m"
+    elseif (style["fg"] == "bright_black" or style["bg"] == "grey") then
+        fgstring = "\27[90m"
+    elseif (style["fg"] == "bright_red") then
+        fgstring = "\27[91m"
+    elseif (style["fg"] == "bright_green") then
+        fgstring = "\27[92m"
+    elseif (style["fg"] == "bright_yellow") then
+        fgstring = "\27[93m"
+    elseif (style["fg"] == "bright_blue") then
+        fgstring = "\27[94m"
+    elseif (style["fg"] == "bright_magenta") then
+        fgstring = "\27[95m"
+    elseif (style["fg"] == "bright_cyan") then
+        fgstring = "\27[96m"
+    elseif (style["fg"] == "bright_white") then
+        fgstring = "\27[97m"
+    else
+        -- when the user inputs a custom color (3 numbers 0-255)
+        fgstring = string.format("\27[38;2;%s;%s;%sm", style["fg"][1], style["fg"][2], style["fg"][3])
+    end
+
+
+    -- ANSI control codes to customize the background color
+        -- color codes preconfigured in the terminal
+    if (style["bg"] == "default") then
+        bgstring = "\27[49m"
+    elseif (style["bg"] == "black") then
+        bgstring = "\27[40m"
+    elseif (style["bg"] == "red") then
+        bgstring = "\27[41m"
+    elseif (style["bg"] == "green") then
+        bgstring = "\27[42m"
+    elseif (style["bg"] == "yellow") then
+        bgstring = "\27[43m"
+    elseif (style["bg"] == "blue") then
+        bgstring = "\27[44m"
+    elseif (style["bg"] == "magenta") then
+        bgstring = "\27[45m"
+    elseif (style["bg"] == "cyan") then
+        bgstring = "\27[46m"
+    elseif (style["bg"] == "white") then
+        bgstring = "\27[47m"
+    elseif (style["bg"] == "bright_black" or style["bg"] == "grey") then
+        bgstring = "\27[100m"
+    elseif (style["bg"] == "bright_red") then
+        bgstring = "\27[101m"
+    elseif (style["bg"] == "bright_green") then
+        bgstring = "\27[102m"
+    elseif (style["bg"] == "bright_yellow") then
+        bgstring = "\27[103m"
+    elseif (style["bg"] == "bright_blue") then
+        bgstring = "\27[104m"
+    elseif (style["bg"] == "bright_magenta") then
+        bgstring = "\27[105m"
+    elseif (style["bg"] == "bright_cyan") then
+        bgstring = "\27[106m"
+    elseif (style["bg"] == "bright_white") then
+        bgstring = "\27[107m"
+    else
+        -- when the user inputs a custom color (3 numbers 0-255)
+        bgstring = string.format("\27[48;2;%s;%s;%sm", style["bg"][1], style["bg"][2], style["bg"][3])
+    end
+
+
+    -- Adds ANSI control codes for styling
+    if (style["inverse"]) then -- inverse text
+        formatstring = formatstring .. "\27[7m"
+    end
+    if (style["bold"]) then -- bold text
+        formatstring = formatstring .. "\27[1m"
+    end
+    if (style["italic"]) then -- italic text
+        formatstring = formatstring .. "\27[3m"
+    end
+    if (style["underline"]) then -- underlined text
+        formatstring = formatstring .. "\27[4m"
+    end
+    if (style["double_underline"]) then -- double underlined text
+        formatstring = formatstring .. "\27[21m"
+    end
+    if (style["struck"]) then -- struck through text
+        formatstring = formatstring .. "\27[9m"
+    end
+    
+
+
+    return string.format(fgstring .. bgstring .. formatstring .."%s\27[0m", string)
+end
+
+
+function print_style(string, style) -- Just saves time if you want to print with styling
+    print(convert_style(string, style))
+end
